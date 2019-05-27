a5e3b0589f360155f5cfc1767f28ed57e40ad8db
tomookakeita <keita516tomo@outlook.jp>
2019-05-27 14:22:35 +0900

    yml,コントローラークラスの作成

--- src/main/java/com/example/NameReceiveController.java
+++ src/main/java/com/example/NameReceiveController.java
@@ -1,5 +1,21 @@
 package com.example;
 
-public class NameReceiveController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/acount")
+public class NameReceiveController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "input-name";
+	}
+
+	@RequestMapping("/name")
+	public String inputName(String name, Model model) {
+		model.addAttribute("name", name);
+		return "output-name";
+	}
 }
