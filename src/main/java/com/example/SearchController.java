f6419595c9f7e3a4036e07b25facebb1f9a763c5
tomookakeita <keita516tomo@outlook.jp>
2019-05-27 18:01:56 +0900

    とりあえずの

--- src/main/java/com/example/SearchController.java
+++ src/main/java/com/example/SearchController.java
@@ -10,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SearchController {
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping("")
 	public String index() {
 		return "user-info-search";
 	}
+
 	@RequestMapping("/userInfo")
-	public String userInfo(Integer id,Model model) {
-		User user=service.load(id);
+	public String userInfo(Integer id, Model model) {
+		User user = service.load(id);
 		model.addAttribute("user", user);
 		return "user-info-view";
 	}
