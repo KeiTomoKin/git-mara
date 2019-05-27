2bd4260f8e5e2576e907bae8c6d368e92103fd4a
tomookakeita <keita516tomo@outlook.jp>
2019-05-27 16:45:19 +0900

    userのドメイン、リポジトリクラスを実装

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,15 +1,25 @@
 package com.example;
 
 public class User {
+	private Integer id;
 	private String name;
-	private Integer age;
-	private String adress;
+	private String age;
+	private String address;
 	
 	
 	
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", age=" + age + ", adress=" + adress + "]";
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + ", getId()=" + getId()
+				+ ", getName()=" + getName() + ", getAge()=" + getAge() + ", getAddress()=" + getAddress()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	public String getName() {
 		return name;
@@ -17,18 +27,20 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
+	public String getAge() {
 		return age;
 	}
-	public void setAge(Integer age) {
+	public void setAge(String age) {
 		this.age = age;
 	}
-	public String getAdress() {
-		return adress;
+	public String getAddress() {
+		return address;
 	}
-	public void setAdress(String adress) {
-		this.adress = adress;
+	public void setAddress(String address) {
+		this.address = address;
 	}
 	
+	
+		
 	
 }
